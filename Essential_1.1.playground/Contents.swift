9835ba9ba691bf5aeb56715aaf9529a2496b152e
Georgios Mastrogiannis <georgios.mastrogiannis@merkle.com>
2023-05-29 17:36:07 +0200

    Option with extensions

--- Essential_1.1.playground/Contents.swift
+++ Essential_1.1.playground/Contents.swift
@@ -1,16 +1,16 @@
 import UIKit
 
-struct LoggedInUser {}
-struct FeedItem {}
-
 // Api Module
 class ApiClient {
     static let shared = ApiClient()
-    func login(completion: (LoggedInUser) -> Void) {}
-    func loadFeed(completion: ([FeedItem]) -> Void) {}
+    func execute(_ : URLRequest, completion: (Data) -> Void) {}
 }
 
 // Login Module
+struct LoggedInUser {}
+extension ApiClient {
+    func login(completion: (LoggedInUser) -> Void) {}
+}
 class LoginViewController: UIViewController {
     var api = ApiClient.shared
     func didTapLogInButton() {
@@ -21,6 +21,10 @@ class LoginViewController: UIViewController {
 }
 
 // Feed Module
+struct FeedItem {}
+extension ApiClient {
+    func loadFeed(completion: ([FeedItem]) -> Void) {}
+}
 class FeedViewController: UIViewController {
     var api = ApiClient.shared
     override func viewDidLoad() {
